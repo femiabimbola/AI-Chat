3c763077852cddabfb812651972379d6ab70a905
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-21 22:08:38 -0800

    added the clipPath to the background

--- app/page.tsx
+++ app/page.tsx
@@ -39,8 +39,12 @@ const Home = () => {
             className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
           >
             <div
+              style={{clipPath: "polygon()"}}
               className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b6] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" /* the left subtle gradient*/
             />
+          </div>
+          <div>
+            <div className="mx-auto max-w-6xl px-6"></div>
           </div>
         </div>
       </div>
