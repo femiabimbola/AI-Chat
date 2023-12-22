84fc206dfb56ca93460b6bd5f2b8f3b24b7b8a44
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-21 23:43:02 -0800

    added picture to the page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Link from "next/link";
 import {ArrowRight} from "lucide-react";
 import {buttonVariants} from "@/components/ui/button";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -40,13 +41,57 @@ const Home = () => {
           >
             <div
               style={{clipPath: "polygon()"}}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b6] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" /* the left subtle gradient*/
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b6] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" /* the left subtle gradient*/
             />
           </div>
+
           <div>
-            <div className="mx-auto max-w-6xl px-6"></div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-6">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <Image
+                    src="/dashboard-preview.jpg"
+                    alt="product review"
+                    width={1365}
+                    height={866}
+                    className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            aria-hidden="true" /* aria-hidden is for decorational stuff , it is the sub */
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+              }}
+              className="relative left-[calc(50%-14rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b6] to-[#9089fc] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]" /* the left subtle gradient*/
+            />
           </div>
         </div>
+      </div>
+
+      {/* Feature Section */}
+      <div className="mx-auto mb-32 mt-32 max-w-5xl sm:mt-56">
+        <div className="mb-12 px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl sm:text-center">
+            <h2 className="mt-2 font-bold text-4xl text-gray-900 sm:text-5xl">
+              Start chatting in seconds with your documents
+            </h2>
+            <p className="mt-4 text-lg text-gray-600">
+              Chatting with your PDF files has never been easier with Chat with
+              PDF
+            </p>
+          </div>
+        </div>
+        {/* The Steps to register */}
+        <ol className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+          <li></li>
+        </ol>
       </div>
     </>
   );
