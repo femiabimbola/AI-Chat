ffc93bf548c29cfc017899c35d587973734939ee
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-22 16:28:04 -0800

    adding navbar

--- app/page.tsx
+++ app/page.tsx
@@ -97,14 +97,58 @@ const Home = () => {
                 Sign up for an account
               </span>
               <span className="mt-2 text-zinc-700">
-                Start out with a free plan or choose our
-                <Link href="/pricing" className="text-blue-600 underline">
+                Start out with a free plan or choose our{" "}
+                <Link
+                  href="/pricing"
+                  className="text-blue-600 underline underline-offset-4"
+                >
                   premium plan
                 </Link>
               </span>
             </div>
           </li>
+
+          {/* The step 2 */}
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600"> Step 2</span>
+              <span className="text-xl font-semibold">
+                Upload your PDF file
+              </span>
+              <span className="mt-2 text-zinc-700">
+                We&apos;ll process your file and make it ready for you to chat
+                with
+              </span>
+            </div>
+          </li>
+
+          <li className="md:flex-1">
+            <div className="flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4">
+              <span className="text-sm font-medium text-blue-600"> Step 3</span>
+              <span className="text-xl font-semibold">
+                Start asking questions
+              </span>
+              <span className="mt-2 text-zinc-700">
+                It&apos;s that simple. Try it out now
+              </span>
+            </div>
+          </li>
         </ol>
+
+        {/* The image  */}
+        <div className="mx-auto max-w-6xl px-6 lg:px-6">
+          <div className="mt-16 flow-root sm:mt-24">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+              <Image
+                src="/file-upload-preview.jpg"
+                alt="Uploading preview "
+                width={1419}
+                height={732}
+                className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div> The navbar</div>;
+};
+
+export default Navbar;
