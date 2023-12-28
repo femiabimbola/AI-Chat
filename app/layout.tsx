16c9959bf52ecb7027f46b381c915ccde9b4286d
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-27 22:04:06 -0800

    design the login link

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -20,6 +21,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
           inter.className
         )}
       >
+        <Navbar />
         {children}
       </body>
     </html>
