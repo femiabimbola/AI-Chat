f456cf282ece90f4aba67b0f4f1dc9c35864d462
Femi Abimbola <femiabimbola6@gmail.com>
2024-03-05 13:24:48 -0800

    ui for upload files

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import {useState} from "react";
-import {Dialog, DialogTrigger} from "@/components/ui/dialog";
+import {Dialog, DialogContent, DialogTrigger} from "@/components/ui/dialog";
 import {Button} from "@/components/ui/button";
 
 /**
@@ -24,6 +24,7 @@ const UploadButton = () => {
       >
         <Button>Upload PDF</Button>
       </DialogTrigger>
+      <DialogContent>hey</DialogContent>
     </Dialog>
   );
 };
